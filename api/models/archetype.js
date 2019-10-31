52837f5688b99591eb9ff7c241511d44663dbff8
Matías Bahamonde <mbahamonde2016@alu.uct.cl>
2019-10-30 22:19:45 -0300

    Correcciones Schema
    adicion de campos meta y format

--- api/models/archetype.js
+++ api/models/archetype.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const archetypeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    meta: JSON,
+    format: String,
     data: JSON
 });
 
